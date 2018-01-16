d7238699d1ca36cd0d92afb7b82dcc5bf13fd94f
Nanda <Nandakumar_Rangasamy@epam.com>
2018-01-16 15:58:09 +0530

    added project

---
+++ practice/jasminetest.js
@@ -0,0 +1,13 @@
+function sum(a,b){
+    return a + b
+}
+
+describe('calc suite', () => {
+    
+    it('test sum', function() {
+       
+        expect(sum(10,10)).toEqual(0);
+            
+    });
+        
+});
